c2e581d43cb830ae0a94b9d2e6ed6c5e03cc4164
Naufil Shaikh <141832951+naufil2002@users.noreply.github.com>
2025-07-18 22:04:33 +0530

    doneeeee

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -30,3 +33,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
